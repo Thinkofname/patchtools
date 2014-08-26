472b149914e4d39f24c92123db04008df22f8588
Thinkofdeath <thinkofdeath@spigotmc.org>
2014-08-26 11:59:32 +0100

    Generalise the ARETURN opcode

--- src/main/java/uk/co/thinkofdeath/patchtools/patch/Instruction.java
+++ src/main/java/uk/co/thinkofdeath/patchtools/patch/Instruction.java
@@ -1,14 +1,14 @@
 package uk.co.thinkofdeath.patchtools.patch;
 
 import org.objectweb.asm.Opcodes;
-import uk.co.thinkofdeath.patchtools.patch.instructions.AReturnInstruction;
 import uk.co.thinkofdeath.patchtools.patch.instructions.InvokeInstruction;
 import uk.co.thinkofdeath.patchtools.patch.instructions.LdcInstruction;
+import uk.co.thinkofdeath.patchtools.patch.instructions.SingleInstruction;
 
 public enum Instruction {
     ANY(null),
     LDC(new LdcInstruction()),
-    ARETURN(new AReturnInstruction()),
+    ARETURN(new SingleInstruction(Opcodes.ARETURN)),
     INVOKE_STATIC(new InvokeInstruction(Opcodes.INVOKESTATIC)),
     INVOKE_SPECIAL(new InvokeInstruction(Opcodes.INVOKESPECIAL)),
     INVOKE_VIRTUAL(new InvokeInstruction(Opcodes.INVOKEVIRTUAL)),
--- src/main/java/uk/co/thinkofdeath/patchtools/patch/instructions/SingleInstruction.java
+++ src/main/java/uk/co/thinkofdeath/patchtools/patch/instructions/SingleInstruction.java
@@ -1,6 +1,5 @@
 package uk.co.thinkofdeath.patchtools.patch.instructions;
 
-import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.InsnNode;
 import uk.co.thinkofdeath.patchtools.PatchScope;
@@ -9,19 +8,26 @@ import uk.co.thinkofdeath.patchtools.patch.InstructionHandler;
 import uk.co.thinkofdeath.patchtools.patch.PatchInstruction;
 import uk.co.thinkofdeath.patchtools.wrappers.ClassSet;
 
-public class AReturnInstruction implements InstructionHandler {
+public class SingleInstruction implements InstructionHandler {
+
+    private int opcode;
+
+    public SingleInstruction(int opcode) {
+        this.opcode = opcode;
+    }
+
     @Override
     public void check(ClassSet classSet, PatchScope scope, PatchInstruction instruction, AbstractInsnNode insn) {
         if (!(insn instanceof InsnNode)) {
             throw new PatchVerifyException();
         }
-        if (insn.getOpcode() != Opcodes.ARETURN) {
+        if (insn.getOpcode() != opcode) {
             throw new PatchVerifyException();
         }
     }
 
     @Override
     public AbstractInsnNode create(ClassSet classSet, PatchScope scope, PatchInstruction instruction) {
-        return new InsnNode(Opcodes.ARETURN);
+        return new InsnNode(opcode);
     }
 }
