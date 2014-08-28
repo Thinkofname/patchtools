7cc480bbb014a3b1463924523157c03422a1928c
Thinkofdeath <thinkofdeath@spigotmc.org>
2014-08-28 19:45:01 +0100

    Add license

--- src/main/java/uk/co/thinkofdeath/patchtools/wrappers/ClassWrapper.java
+++ src/main/java/uk/co/thinkofdeath/patchtools/wrappers/ClassWrapper.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Matthew Collins
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.co.thinkofdeath.patchtools.wrappers;
 
 import org.objectweb.asm.tree.ClassNode;
@@ -50,6 +66,7 @@ public class ClassWrapper {
 
     // Shouldn't ever been updated so we cache
     private MethodWrapper[] methodCache;
+
     public MethodWrapper[] getMethods(boolean stripHidden) {
         if (stripHidden) {
             if (methodCache == null) {
@@ -82,6 +99,7 @@ public class ClassWrapper {
 
     // Shouldn't ever been updated so we cache
     private FieldWrapper[] fieldCache;
+
     public FieldWrapper[] getFields(boolean stripHidden) {
         if (stripHidden) {
             if (fieldCache == null) {
