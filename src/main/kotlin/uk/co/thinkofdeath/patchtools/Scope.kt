Thinkofname/patchtools

commit a998fe8eb62d2ecfc907ef4394933430e7b7bec2

Add a way to reapply a existing scope

src/main/kotlin/uk/co/thinkofdeath/patchtools/Scope.kt
@@ -55,7 +55,7 @@ class PatchScope(private val parent: PatchScope? = null) {
         return classMappings
             .filterValues { it == cls }
             .map { it.getKey() }
-            .first
+            .first()
     }
 
     fun putMethod(mw: MethodWrapper, name: String, desc: String): Boolean {
@@ -71,7 +71,7 @@ class PatchScope(private val parent: PatchScope? = null) {
         return methodMappings.keySet()
             .filter { it.has(owner) }
             .filter { getMethodDesc(it).equals(joined) }
-            .first ?: parent?.getMethod(owner, name, desc)
+            .first() ?: parent?.getMethod(owner, name, desc)
     }
 
     private fun getMethodDesc(mw: MethodWrapper): String? {
@@ -95,7 +95,7 @@ class PatchScope(private val parent: PatchScope? = null) {
         return fieldMappings.keySet()
             .filter { it.has(owner) }
             .filter { getFieldDesc(it).equals(joined) }
-            .first ?: parent?.getField(owner, name, desc)
+            .first() ?: parent?.getField(owner, name, desc)
     }
 
     private fun getFieldDesc(mw: FieldWrapper): String? {

src/main/kotlin/uk/co/thinkofdeath/patchtools/Patcher.kt
@@ -24,6 +24,7 @@ import java.nio.charset.StandardCharsets
 import uk.co.thinkofdeath.patchtools.patch.PatchClasses
 import uk.co.thinkofdeath.patchtools.matching.MatchGenerator
 import uk.co.thinkofdeath.patchtools.lexer.Lexer
+import uk.co.thinkofdeath.patchtools.logging.StateLogger
 
 class Patcher(val classes: ClassSet) {
     {
@@ -51,5 +52,18 @@ class Patcher(val classes: ClassSet) {
             it.apply(foundScope, classes)
         }
         return foundScope
+    }
+
+    fun reapply(patchClasses: PatchClasses, patchScope: PatchScope) {
+        val logger = StateLogger()
+        // Slightly faster to do it this way since the instruction checking is the heaviest
+        patchClasses.classes.all { it.checkAttributes(logger, patchScope, classes) }
+            && patchClasses.classes.all { it.checkFields(logger, patchScope, classes) }
+            && patchClasses.classes.all { it.checkMethods(logger, patchScope, classes) }
+            && patchClasses.classes.all { it.checkMethodsInstructions(logger, patchScope, classes) }
+
+        patchClasses.classes.forEach {
+            it.apply(patchScope, classes)
+        }
     }
 }

src/main/kotlin/uk/co/thinkofdeath/patchtools/patch/PatchClasses.kt
@@ -65,7 +65,7 @@ public class PatchClasses(reader: Lexer) {
     public fun getClass(name: String): PatchClass? {
         return classes
             .filter { it.ident.name == name }
-            .first
+            .first()
     }
 
     internal fun import(cls: String) {

commit 4f7c1476f20c5b39bbddf0bd398e806b0063d9db

Fix a bug introduced in a998fe8eb62d2ecfc907ef4394933430e7b7bec2

src/main/kotlin/uk/co/thinkofdeath/patchtools/Scope.kt
@@ -71,7 +71,7 @@ class PatchScope(private val parent: PatchScope? = null) {
         return methodMappings.keySet()
             .filter { it.has(owner) }
             .filter { getMethodDesc(it).equals(joined) }
-            .first() ?: parent?.getMethod(owner, name, desc)
+            .firstOrNull() ?: parent?.getMethod(owner, name, desc)
     }
 
     private fun getMethodDesc(mw: MethodWrapper): String? {
@@ -95,7 +95,7 @@ class PatchScope(private val parent: PatchScope? = null) {
         return fieldMappings.keySet()
             .filter { it.has(owner) }
             .filter { getFieldDesc(it).equals(joined) }
-            .first() ?: parent?.getField(owner, name, desc)
+            .firstOrNull() ?: parent?.getField(owner, name, desc)
     }
 
     private fun getFieldDesc(mw: FieldWrapper): String? {